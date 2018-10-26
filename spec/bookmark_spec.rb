955d8ca242d2e0bf1257a92474f4b6b8d68158f7
Josh Harris <joshharris@joshs-mbp.home>
2018-10-26 13:01:16 +0100

    Fails all 3 examples at step 10

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,13 +1,18 @@
-require 'bookmark'
+require 'pg'
 
-describe Bookmark do
-  describe '.all' do
-  it 'returns all bookmarks' do
-  bookmarks = Bookmark.all
+feature 'Viewing bookmarks' do
+  scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
 
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.destroyallsoftware.com")
-      expect(bookmarks).to include("http://www.google.com")
-    end
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
+    visit('/bookmarks')
+
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
   end
 end
